6084f76d01f300894d32d2ea4fd29efad8205f63
Brian Szwec <briaanszwec@gmail.com>
2023-03-20 17:46:59 -0300

    TERCERA ENTREGA

--- Scripts/scriptsPages/javCarrito.js
+++ Scripts/scriptsPages/javCarrito.js
@@ -10,20 +10,27 @@ function agregar_a_carrito(e){
     
     let etiqueta = e.target;
     let div = etiqueta.parentNode;
-    
+   
     
     let nombre_producto = div.querySelector(".h2card").textContent;
     let precio_producto = div.querySelector(".p-card-precio").textContent;
-    let cantidad = div.querySelector(".cantidad").textContent;
+    let cantidad = div.querySelector(".cantidad").value;
+
+    let producto_existente = carritoA.find(p => p.nombre === nombre_producto && p.precio === precio_producto);
+    if (producto_existente) {
+      producto_existente.cantidad += 1;
+  } else {
+      // Si el producto no está en el carrito, agrégalo con cantidad 1
+      let producto = {
+          nombre: nombre_producto,
+          precio: precio_producto,
+          cantidad: 1,
+        };
+        
+      carritoA.push(producto);
+  }
 
 
-let producto = {
-    nombre: nombre_producto,
-    precio: precio_producto,
-    cantidad: cantidad,
-}
-
-carritoA.push(producto);
 console.log( carritoA)
 
 let productos_json = JSON.stringify(carritoA);
@@ -44,27 +51,65 @@ function render_carrito(){
 
   for(let producto of carritoA){
     let linea = document.createElement('tr');
-
+    
     linea.innerHTML = `
     <td><p>${producto.nombre}</p></td>
-    <td>${producto.cantidad}</td>
     <td>${producto.precio}</td>
-    <td><button class="btn btn-danger borrar_elemento">Borrar</button></td>
+    <td>${producto.cantidad}</td>
+    <td><button class="btn btn-danger borrar">Borrar</button></td>
     `
     cuerpo.append(linea);
+
+
+   
   }
 
+  let btn_borrar = document.querySelectorAll('.borrar');
+  
+  for(let boton of btn_borrar){
+    boton.addEventListener('click', borrar_producto);
+  }
+  
+ 
+}
+
+
+
+function borrar_producto(e){
+  
+  let div = e.target.parentNode.parentNode;
+
+  let producto_a_eliminar = div.querySelectorAll('tr').textContent;
   
 
+  
+  
+  
+  function  eliminar_producto(producto){
+
+    return producto.nombre != producto_a_eliminar;
+  }
+  
+  
+  let resultado_filter = carritoA.filter(eliminar_producto)
+  
+ 
+  console.log(carritoA)
+  console.log(resultado_filter)
+  
+ 
+  carritoA = resultado_filter;
+
+ div.remove();
 }
 
 
 
 
+
+
+
 let btn_compra = document.querySelectorAll('.card-button');
-
-
-
 for(let boton of btn_compra){
     boton.addEventListener('click' , agregar_a_carrito)
 }
