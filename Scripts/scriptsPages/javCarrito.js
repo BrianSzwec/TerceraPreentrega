c5fec8c3aca22eeff8791e43cb919e0b593ffbaa
Brian Szwec <briaanszwec@gmail.com>
2023-04-02 22:45:20 -0300

    finaljs

--- Scripts/scriptsPages/javCarrito.js
+++ Scripts/scriptsPages/javCarrito.js
@@ -1,23 +1,24 @@
-
-
 ///////////////////////////////////
 let carritoA = [];
 let total = 0;
-
+let cantidadIni =1;
+if(localStorage.getItem("carritoA")) {
+  carritoA = JSON.parse(localStorage.getItem("carritoA"));
+  render_carrito();
+}
 
 ///////////AGREGAR A CARRITO
 function agregar_a_carrito(e){
     
-    let etiqueta = e.target;
-    let div = etiqueta.parentNode;
-   
-    
-    let nombre_producto = div.querySelector(".h2card").textContent;
-    let precio_producto = div.querySelector(".p-card-precio").textContent;
-    let cantidad = div.querySelector(".cantidad").value;
+  let etiqueta = e.target;
+  let div = etiqueta.parentNode;
+ 
+  let nombre_producto = div.querySelector(".h2card").textContent;
+  let precio_producto = parseFloat(div.querySelector(".p-card-precio").textContent);
+  let cantidad = div.querySelector(".cantidad").value;
 
-    let producto_existente = carritoA.find(p => p.nombre === nombre_producto && p.precio === precio_producto);
-    if (producto_existente) {
+  let producto_existente = carritoA.find(p => p.nombre === nombre_producto && p.precio === precio_producto);
+  if (producto_existente) {
       producto_existente.cantidad += 1;
   } else {
       // Si el producto no está en el carrito, agrégalo con cantidad 1
@@ -25,23 +26,25 @@ function agregar_a_carrito(e){
           nombre: nombre_producto,
           precio: precio_producto,
           cantidad: 1,
-        };
-        
+      };
+      
       carritoA.push(producto);
   }
 
+  total = carritoA.reduce((acc, curr) => acc + (curr.precio * curr.cantidad), 0);
 
-console.log( carritoA)
+  console.log( carritoA)
+
+  let productos_json = JSON.stringify(carritoA);
+  localStorage.setItem("carritoA", productos_json);
+
+  FuncabrirCarrito()
+
+  render_carrito()   
+}
 
 let productos_json = JSON.stringify(carritoA);
 localStorage.setItem("carritoA", productos_json);
-
-FuncabrirCarrito()
-
-render_carrito()
-    
-}
-
 
 ///////////MOSTRAR CARRITO Y RENDERIZAR PRODS
 function render_carrito(){
@@ -71,7 +74,23 @@ function render_carrito(){
     boton.addEventListener('click', borrar_producto);
   }
   
- 
+  let total = 0;
+  for (let producto of carritoA) {
+
+    total +=  producto.precio * producto.cantidad;
+  }
+  
+  // Crear la línea de total
+  let lineaTotal = document.createElement('tr');
+  lineaTotal.innerHTML = `
+    <td></td>
+    <td></td>
+    <td>Total:</td>
+    <td>${total}</td>
+  `;
+  
+  // Agregar la línea de total al cuerpo de la tabla
+  cuerpo.append(lineaTotal);
 }
 
 
@@ -107,31 +126,6 @@ for(let boton of btn_compra){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ////FUNCIONES BOTON CARRITO
 let abrirCarrito = document.getElementById('boton_carrito');
 function FuncabrirCarrito(){
@@ -149,5 +143,3 @@ function FunccerrarCarrito (){
     
 }
 cerrarCarrito.addEventListener('click' , FunccerrarCarrito);
-
-
